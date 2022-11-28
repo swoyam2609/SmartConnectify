0f3f4b84b3f059d9f23bd7d91cb44686524f8701
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2022-11-29 04:17:22 +0530

    Sign in page completed

--- app/src/main/java/com/example/smartconnectify/SignUpSignInActivity.kt
+++ app/src/main/java/com/example/smartconnectify/SignUpSignInActivity.kt
@@ -1,11 +1,28 @@
 package com.example.smartconnectify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smartconnectify.databinding.ActivitySignUpSignInBinding
 
 class SignUpSignInActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySignUpSignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up_sign_in)
+        binding = ActivitySignUpSignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signInButotn.setOnClickListener {
+            val i = Intent(this, SignInActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(
+                com.google.android.material.R.anim.abc_fade_in,
+                com.google.android.material.R.anim.abc_fade_out)
+            finish()
+        }
+
+
+
+
     }
 }
