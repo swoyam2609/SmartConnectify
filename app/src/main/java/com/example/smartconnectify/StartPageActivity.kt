f19b3fc2e5715ce21ec977fcef7fc9a6ece7080f
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2022-11-29 03:27:19 +0530

    Sign Up and Sign In option page complete

--- app/src/main/java/com/example/smartconnectify/StartPageActivity.kt
+++ app/src/main/java/com/example/smartconnectify/StartPageActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smartconnectify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -36,5 +37,31 @@ class StartPageActivity : AppCompatActivity() {
         handler.postDelayed({
             binding.bgDocImage.setImageResource(imageArray[2])
         }, 18000)
+        handler.postDelayed({
+            binding.bgDocImage.setImageResource(imageArray[0])
+        }, 27000)
+        handler.postDelayed({
+            binding.bgDocImage.setImageResource(imageArray[1])
+        }, 30000)
+        handler.postDelayed({
+            binding.bgDocImage.setImageResource(imageArray[2])
+        }, 33000)
+        handler.postDelayed({
+            binding.bgDocImage.setImageResource(imageArray[0])
+        }, 36000)
+        handler.postDelayed({
+            binding.bgDocImage.setImageResource(imageArray[1])
+        }, 39000)
+        handler.postDelayed({
+            binding.bgDocImage.setImageResource(imageArray[2])
+        }, 42000)
+        binding.nextButton.setOnClickListener {
+            val i = Intent(this, SignUpSignInActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(
+                com.google.android.material.R.anim.abc_fade_in,
+                com.google.android.material.R.anim.abc_fade_out)
+            finish()
+        }
     }
 }
