f8fb94358b751bf25c0b9ea50a54a32e2ee31b60
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2022-11-29 14:35:17 +0530

    Sign Up page in making

--- app/src/main/java/com/example/smartconnectify/SignUpActivity.kt
+++ app/src/main/java/com/example/smartconnectify/SignUpActivity.kt
@@ -2,10 +2,15 @@ package com.example.smartconnectify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smartconnectify.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    lateinit var binding : ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
+
+
     }
 }
