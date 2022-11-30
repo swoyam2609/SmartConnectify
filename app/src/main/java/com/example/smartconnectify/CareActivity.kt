be8295f495caa720a4a6d49471b9dc9c5c446b58
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2022-11-30 14:36:31 +0530

    Care Page Initiated

--- app/src/main/java/com/example/smartconnectify/CareActivity.kt
+++ app/src/main/java/com/example/smartconnectify/CareActivity.kt
@@ -2,10 +2,15 @@ package com.example.smartconnectify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smartconnectify.databinding.ActivityCareBinding
 
 class CareActivity : AppCompatActivity() {
+    lateinit var binding : ActivityCareBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_care)
+        binding = ActivityCareBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
