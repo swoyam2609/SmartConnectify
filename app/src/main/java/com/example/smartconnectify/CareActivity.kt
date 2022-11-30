29224df8742bd5233a9601b4fa82eb6614749144
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2022-11-30 13:19:22 +0530

    Home Page Activity Complete

---
+++ app/src/main/java/com/example/smartconnectify/CareActivity.kt
@@ -0,0 +1,11 @@
+package com.example.smartconnectify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CareActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_care)
+    }
+}
--- app/src/main/java/com/example/smartconnectify/MainActivity.kt
+++ app/src/main/java/com/example/smartconnectify/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.smartconnectify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smartconnectify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.careicon.setOnClickListener {
+            val i = Intent(this, CareActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(com.google.android.material.R.anim.abc_fade_in, com.google.android.material.R.anim.abc_fade_out)
+        }
     }
 }
