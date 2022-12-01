377d13fa8d12fcd477a7e0271dadd0e9f4766b5a
Swoyam Siddharth Nayak <swoyamsiddharthnayak@gmail.com>
2022-12-01 19:23:16 +0530

    Maps linked

--- app/src/main/java/com/example/smartconnectify/HospitalNearbyActivity.kt
+++ app/src/main/java/com/example/smartconnectify/HospitalNearbyActivity.kt
@@ -1,5 +1,7 @@
 package com.example.smartconnectify
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.smartconnectify.databinding.ActivityHospitalNearbyBinding
@@ -10,5 +12,29 @@ class HospitalNearbyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHospitalNearbyBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.sumLoc.setOnClickListener {
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://goo.gl/maps/twHbJAiooEsPtZBL9")
+            startActivity(openURL)
+        }
+
+        binding.amriLoc.setOnClickListener {
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://goo.gl/maps/awH8Zajd2kEWhYnG6")
+            startActivity(openURL)
+        }
+
+        binding.aiimsLoc.setOnClickListener {
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://goo.gl/maps/SwYCpunYXSAjN3ZHA")
+            startActivity(openURL)
+        }
+
+        binding.apoloLoc.setOnClickListener {
+            val openURL = Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data = Uri.parse("https://goo.gl/maps/ZuBh9uCaqCseuFQT9")
+            startActivity(openURL)
+        }
     }
 }
